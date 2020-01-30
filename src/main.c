3f4cdde7bd04f0685b36c9f301d1343d0b004003
robinbutel <robin.butel@hotmail.com>
2020-01-30 11:06:30 +0100

    First RB commit

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  * \file main.c
  * \brief Main program for TacticsArena
  * \details Contains main function
- * \author Thibault DOUCET
+ * \author Thibault DOUCET Robin BUTEL
  * \version 0.0.1
  * \date 28/01/2020
  */

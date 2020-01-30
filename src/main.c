211ba0b608101112268c10b84e167ef1437ca918
robinbutel <robin.butel@hotmail.com>
2020-01-30 11:07:45 +0100

    changed authors

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
  * \file main.c
  * \brief Main program for TacticsArena
  * \details Contains main function
- * \author Thibault DOUCET Robin BUTEL
+ * \author Thibault DOUCET
  * \version 0.0.1
  * \date 28/01/2020
  */

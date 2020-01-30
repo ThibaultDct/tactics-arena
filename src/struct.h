e3627bf41b343c2a55855ad1b40b7aac1333b18b
robinbutel <robin.butel@hotmail.com>
2020-01-30 11:22:17 +0100

    Definition of basic entity structure

--- src/struct.h
+++ src/struct.h
@@ -1,24 +1,13 @@
 #ifndef struct_h
 #define struct_h
 
-typedef struct t_player
-{
-    int classe;
-} Player;
-
-typedef struct t_mob
-{
-    int type;
-} Mob;
-
 typedef struct t_entity
 {
-    int pdv;
-    union
-    {
-        Player player;
-        Mob mob;
-    };
+    int id;
+    char character_name[25];
+    char character_class;
 } Entity;
+
+
 
 #endif

4ec37d889ecff0fff09a6c085951b1639871dd07
robinbutel <robin.butel@hotmail.com>
2020-02-04 10:04:14 +0100

    creation common.h

--- src/common.h
+++ src/common.h
@@ -1,6 +1,13 @@
 #ifndef common_h
 #define common_h
 
-typedef enum {OK, ERROR_1, ERROR_2} err_t;
+#define STR_SHORT 25
+#define STR_LONG 50
+#define NUM_ERROR 3
+
+typedef enum {OK, POINTER_NULL, ERROR} err_t;
+
+char error_message[NUM_ERROR][STR_LONG] = {"\0", "ERROR: Pointer is Null\n", "ERROR\n"};
+
 
 #endif
--- src/struct.c
+++ src/struct.c
@@ -7,7 +7,16 @@
  * \date 04/02/2020
  */
 
-#include <file.h>
 #include <stdlib.h>
 #include "struct.h"
+#include "common.h"
 
+err_t get_classes(Class all_classes[NUM_CLASS])
+{
+    FILE *fp = NULL;
+    fp=fopen("classes","r");
+    if(fp==NULL) return ERROR_1;
+    fread(all_classes, sizeof(Class), NUM_CLASS, fp);
+    fclose(fp);
+    
+}

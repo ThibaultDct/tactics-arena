5dd94f0ff0023208a22c2c550361e3454d20dfe9
unknown <robin.butel@hotmail.com>
2020-02-09 22:12:16 +0100

    Update header structure

--- outils/outil_creation.c
+++ outils/outil_creation.c
@@ -1,5 +1,5 @@
 /**
- * \file creation.c
+ * \file outil_creation.c
  * \brief program to save data in files
  * \details Contains program to save data structures in files
  * \author Robin BUTEL
@@ -8,18 +8,19 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
-#include "struct.h"
-#include "common.h"
+#include "../src/struct.h"
+#include "../src/common.h"
 
 err_t create_class(Class * class)
 {
     FILE *fp = NULL;
-    fp=fopen("tactics_arena/data/classes","a");
-    if(fp==NULL) return POINTER_NULL;
+    if(fp=fopen("tactics_arena/data/classes","a")==NULL) return POINTER_NULL;
+    
     fclose(fp);
 }
 
 int main()
 {
+
     return 0;
 }

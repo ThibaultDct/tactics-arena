04826458dc00bdb889182840a1684acba854dbfa
Doucet Thibault <thibault.doucet44@gmail.com>
2020-02-14 10:55:16 +0100

    Added load and free textures, still bugs

--- src/graphics.h
+++ src/graphics.h
@@ -6,8 +6,11 @@
 #include "../SDL2/include/SDL2/SDL_ttf.h"
 #include "audio.h"
 
+void loadMenuTextures(SDL_Renderer *renderer);
+void freeTextures(void);
 void displayText(SDL_Renderer *renderer, TTF_Font *police, int x, int y, int size, char *content, char *text_police, int r, int g, int b);
-int displaySprite(SDL_Renderer *renderer, char *sprite, int x, int y);
+//int displaySprite(SDL_Renderer *renderer, char *sprite, int x, int y);
+int displaySprite(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y);
 int closeWindow(SDL_Window *pWindow);
 int createGameWindow(int x, int y);
 int displayMenu(int x, int y);

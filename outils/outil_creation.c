c7eed37562dc88276b90e0264bd1fdeb3318e528
robinbutel <robin.butel@hotmail.com>
2020-02-14 11:58:17 +0100

    UPDATE TO CREATION OUTIL

--- outils/outil_creation.c
+++ outils/outil_creation.c
@@ -8,27 +8,43 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "../src/struct.h"
 #include "../src/common.h"
 
 err_t create_class(Class * class)
 {
-    FILE *fp = NULL;
-    char * path = malloc(sizeof(char)*15);
+    FILE * fp;
+    char * path = malloc(sizeof(char)*20);
     char num[3];
 
     itoa(class->cla_id, num, 10);
-    sprintf(path, "../data/%s",num);
+    sprintf(path, "../data/%s_%s",num,class->cla_name);
+    printf("\n%s\n",path);
 
-    if(fp=fopen(path,"ab")==NULL) return POINTER_NULL;
-    fwrite(class, sizeof(Class), 1, fp);
+    if((fp=fopen(path,"wb"))==NULL) return POINTER_NULL;
+
+    if(fwrite(class, sizeof(Class),1,fp)!=1) printf("%s\n",strerror(errno));
+
+    free(path);
     fclose(fp);
+    return OK;
 }
 
 int main()
 {
-  //Class class = {Ranger};
+    Class class = {Ranger,"Ranger",{0,1,2,3,4,5},NULL};
+    printf("%s\n",error_message[create_class(&class)]);
 
-    create_class(&class);
+    Class * read = malloc(sizeof(Class));
+
+    FILE *fp;
+
+    fp=fopen("../data/1_Ranger","rb");
+    fread(read, sizeof(Class), 1, fp);
+    printf("%d\n%s\n%d %d %d %d %d %d\n",read->cla_id,read->cla_name,read->basic_stats[0],read->basic_stats[1],read->basic_stats[2],read->basic_stats[3],read->basic_stats[4],read->basic_stats[5]);
+    fclose(fp);
+    free(read);
     return 0;
 }
--- src/common.h
+++ src/common.h
@@ -5,8 +5,8 @@
 #define STR_LONG 50
 #define NUM_ERROR 4
 
-typedef enum {OK, POINTER_NULL, ERROR, SDL_ERROR} err_t;
+typedef enum {OK, POINTER_NULL, SDL_ERROR} err_t;
 
-char error_message[NUM_ERROR][STR_LONG] = {"\0", "ERROR: Pointer is Null\n", "ERROR\n", "ERROR : SDL\n"};
+char error_message[NUM_ERROR][STR_LONG] = {"OK\n", "ERROR: Pointer is Null\n", "ERROR : SDL\n"};
 
 #endif

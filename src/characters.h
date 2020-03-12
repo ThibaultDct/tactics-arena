535ae81217dfefe37c43f5bc4fe7f6efcbfad600
LucienCH <chauvin.lucien.perso@gmail.com>
2020-03-12 10:05:37 +0100

    working version

--- src/characters.h
+++ src/characters.h
@@ -2,7 +2,6 @@
 #define characters_h
 
 #include "struct.h"
-#include "common.h"
 
 int displayCharacters(SDL_Renderer * renderer, Tile * grid, int x, int y);
 int setEntityToTile(Tile * grid, Entity * entity, Coord tile, int xSize, int ySize);
